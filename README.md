f97b3c22c63db7bef51ba953a2d4c1cd194fac81
Stanley Wu <stanleyzqwu@gmail.com>
2020-12-29 18:11:07 -0500

    re-arrange and modified elements in index.html, styles.css, use jquery in index.js, add comments

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Course Map Frontend
-
+This is the frontend for the Course Map Prerequisite web app. It displays the prerequisite courses needed to take desired courses in McGill.
+User inputs are sent to <https://course-map.api.tianshome.com/>.
+This backend generates a graph using python Dash before being rendered on the page by an iFrame.
+Original frontend: <https://github.com/jacobthebanana/course-map-frontend>
+Original app: <https://github.com/jacobthebanana/mcgill-course-map/tree/dockerable>
--- index.js
+++ index.js
@@ -1,37 +1,35 @@
-
+// prevents the form from refreshing the page when user clicks on submit button
 $("#user-input-form").submit(function(e) {
   e.preventDefault();
 });
 
-document.getElementById("submit").addEventListener("click", () => {
-    let desiredCourses = document.getElementsByTagName("input")[0].value;
-    let exemptCourses = document.getElementsByTagName("input")[1].value;
-
-    console.log(desiredCourses);
-    console.log(exemptCourses);
-
+// when user clicks on submit, parse the input and update src attribute of the iframe
+$("#submit").click(() => {
+    // get the user inputs
+    let desiredCourses = $("input")[0].value;
+    let exemptCourses = $("input")[1].value;
+    // parse the user inputs
     let parsedDesiredCourses = parser(desiredCourses);
     let parsedExemptCourses = parser(exemptCourses);
-    
+    // format the url/src attribute of the iframe
     let link = `https://course-map.api.tianshome.com/?courses=${parsedDesiredCourses}&courses_excluded=${parsedExemptCourses}`;
-    console.log(link);
-    let cm_iframe = document.getElementById("course-map-iframe")
-    cm_iframe.setAttribute("src", link);
-    cm_iframe.add("display-course-map-iframe").remove("no-display");
+    // update the attribute, add the "display-course-map-iframe" class, and remove the "no-border" class
+    $("#course-map-iframe").attr("src", link).addClass("display-course-map-iframe").removeClass("no-border");
 });
 
-// function that parses user input into right format --> 
-// COURSE_PREFIX+NUMBER,
+// function that parses user input into right format for the link --> 
+// example 1: MATH 236, COMP 302
+// output 1:  MATH+236,+COMP+302
+// example 2: MATH 139
+// output 2: MATH+139
 function parser(userInput) {
-  tokenList = [];
-  userInputList = userInput.split(" ");
+  tokenList = []; // collects/inserts tokens
+  userInputList = userInput.split(" "); // seperate user input by space
   len = userInputList.length - 1;
   userInputList.forEach((val, index) => {
-    if (index == len) {
-      // end
+    if (index == len) { // end
       tokenList.push(val);
-    } else if (val == " ") {
-      // space
+    } else if (val == " ") {// space: add "+" token
       tokenList.push("+")
     } else {
       let isComma = val.slice(val.length - 1); // returns the last character of the string
@@ -40,12 +38,10 @@ function parser(userInput) {
         tokenList.push(val);
         tokenList.push("+");
       } else {
-        if (isNaN(val)) {
-          // case where val = course prefix (ex: MATH)
+        if (isNaN(val)) {// case where the token is a letter => val = course prefix (ex: MATH)
           tokenList.push(val);
           tokenList.push("+");
-        } else {
-          // case where val = number without comma at end (ex: 240)
+        } else {// case where val = number without comma at end (ex: 240)
           tokenList.push(val);
         }
       }
